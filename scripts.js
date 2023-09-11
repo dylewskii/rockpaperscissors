92866890923c1301b0f003eeb16741f0c193b126
dyskio <130579182+dyskio@users.noreply.github.com>
2023-09-11 15:11:49 +0200

    add playRound function

--- scripts.js
+++ scripts.js
@@ -1,9 +1,33 @@
-
 let getComputerChoice = function () {
     const options = ['Rock', 'Paper', 'Scissors'];
     const randomIndex = Math.floor(Math.random() * options.length);
     const choice = options[randomIndex];
-    console.log(choice);
+    return choice;
 }
 
-getComputerChoice();
+let playRound = function (playerSelection, computerSelection) {
+    if (!playerSelection || !computerSelection) {
+        return "Please provide both playerSelection and computerSelection.";
+    }
+
+    const player = playerSelection.toLowerCase();
+    const pc = computerSelection.toLowerCase();
+
+    if (player === pc){
+        return "tie";
+    } else if (
+        (player === "rock" && pc === "scissors") ||
+        (player === "paper" && pc === "rock") ||
+        (player === "scissors" && pc === "paper")
+    ) {
+        return "player wins";
+    } else {
+        return "pc wins";
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+playRound();
