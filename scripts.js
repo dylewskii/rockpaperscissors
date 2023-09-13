dec2e059e5a10016459410a879600069067ae2aa
dyskio <130579182+dyskio@users.noreply.github.com>
2023-09-13 10:22:12 +0200

    fixed game functionality

--- scripts.js
+++ scripts.js
@@ -1,58 +1,76 @@
+// Keep track of player / computer score
+let playerScore = 0;
+let computerScore = 0;
+
+// Return computer's choice from Rock, Paper, Scissors array
 let getComputerChoice = function () {
-    const options = ['Rock', 'Paper', 'Scissors'];
+    const options = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * options.length);
     const choice = options[randomIndex];
     return choice;
 }
 
+// Play a round - return winner/loser/tie
 let playRound = function (playerSelection, computerSelection) {
-    const player = playerSelection.toLowerCase();
-    const pc = computerSelection.toLowerCase();
+    const playerSelectionLower = playerSelection.toLowerCase();
+    const computerSelectionLower = computerSelection.toLowerCase();
 
-    if (player === pc){
-        alert("Tie!")
+    if (playerSelectionLower === computerSelectionLower) {
+        $("h1").text("It's a tie!");
         return "tie";
     } else if (
-        (player === "rock" && pc === "scissors") ||
-        (player === "paper" && pc === "rock") ||
-        (player === "scissors" && pc === "paper")
+        (playerSelectionLower === "rock" && computerSelectionLower === "scissors") ||
+        (playerSelectionLower === "paper" && computerSelectionLower === "rock") ||
+        (playerSelectionLower === "scissors" && computerSelectionLower === "paper")
     ) {
-        alert("Player Wins!")
+        $("h1").text("You used " + playerSelection);
         return "player";
     } else {
-        alert("PC Wins!")
-        return "pc";
+        $("h1").text("Computer used " + computerSelection);
+        return "computer";
     }
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
-let game = function () {
-    let gamesPlayed = 1;
-
-    while (gamesPlayed <= 5) {
-        alert("Game Number: " + gamesPlayed);
-        const playerSelection = prompt("Choose rock/paper/scissors: ");
-        const computerSelection = getComputerChoice();
-
-        const result = playRound(playerSelection, computerSelection);
-
-        if (result === "player"){
-            playerScore++;
-        } else if (result === "pc"){
-            computerScore++;
-        }
-        gamesPlayed++;
+// Update scores and check for game over
+function updateScores(result) {
+    if (result === "player") {
+        playerScore++;
+    } else if (result === "computer") {
+        computerScore++;
     }
 
+    $("#player-score").text(playerScore);
+    $("#computer-score").text(computerScore);
+
+    if (playerScore === 5 || computerScore === 5) {
+        endGame();
+    } 
+}
+
+// End the game and display the winner
+function endGame() {
     if (playerScore > computerScore) {
-        alert("Player wins the match with a score of " + playerScore + " to " + computerScore);
+        $("h1").text("You win the match with a score of " + playerScore + " to " + computerScore);
     } else if (computerScore > playerScore) {
-        alert("PC wins the match with a score of " + computerScore + " to " + playerScore);
+        $("h1").text("Computer wins the match with a score of " + computerScore + " to " + playerScore);
     } else {
-        alert("It's a tie with a score of " + playerScore + " to " + computerScore);
+        $("h1").text("It's a tie with a score of " + playerScore + " to " + computerScore);
     }
+
+    // reset the game
+    playerScore = 0;
+    computerScore = 0;
+    $("#player-score").text(playerScore);
+    $("#computer-score").text(computerScore);
 }
 
-game();
+// Function to handle button click
+function handleButtonClick(event) {
+    const playerSelection = event.target.id.toLowerCase();
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+    updateScores(result);
+}
+
+// Add event listeners to the buttons
+$(".btn").on("click", handleButtonClick);
